2c2a2bc5cc10ac7b6bbcdf8c2e9145de6e001a60
obemeuche <obemeuchechi@gmail.com>
2022-12-26 10:25:10 +0100

    Customer class

---
+++ customer/src/main/java/com/obeme/customer/Customer.java
@@ -0,0 +1,14 @@
+package com.obeme.customer;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Customer {
+
+    private Integer Id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
