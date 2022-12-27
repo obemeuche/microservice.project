67adda7dad79b3c16092f39f67897ecff1bf0e3d
obemeuche <obemeuchechi@gmail.com>
2022-12-28 00:01:06 +0100

    added database configurations and docker-compose.yml

--- customer/src/main/java/com/obeme/customer/Customer.java
+++ customer/src/main/java/com/obeme/customer/Customer.java
@@ -1,11 +1,25 @@
 package com.obeme.customer;
 
-import lombok.Builder;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
+@ToString
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private Integer Id;
     private String firstName;
     private String lastName;
