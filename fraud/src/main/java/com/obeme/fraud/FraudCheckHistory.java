3934f677c2e2bf8fcc253b42a5c8cf0be5c9e607
obemeuche <obemeuchechi@gmail.com>
2022-12-29 15:00:04 +0100

    created a FraudCheckHistory entity

---
+++ fraud/src/main/java/com/obeme/fraud/FraudCheckHistory.java
@@ -0,0 +1,29 @@
+package com.obeme.fraud;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class FraudCheckHistory {
+    @Id
+    @SequenceGenerator(
+            name = "fraud_id_sequence",
+            sequenceName = "fraud_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "fraud _id_sequence"
+    )
+    private Integer id;
+    private Integer customerId;
+    private Boolean isFraudster;
+    private LocalDateTime createdAt;
+}
