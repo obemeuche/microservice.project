f2538dc16b8bfdd90d74a1e3fc83d668447c5991
obemeuche <obemeuchechi@gmail.com>
2022-12-29 16:53:50 +0100

    used rest to communicate with the fraud service using RestTemplate

--- customer/src/main/java/com/obeme/customer/CustomerService.java
+++ customer/src/main/java/com/obeme/customer/CustomerService.java
@@ -1,9 +1,12 @@
 package com.obeme.customer;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer
                 .builder()
@@ -14,8 +17,21 @@ public record CustomerService(CustomerRepository customerRepository) {
 
         //todo: check if email is valid
         //todo: check if email is taken
+        List<Customer> allCustomers = customerRepository.findAll();
+        for (Customer findCustomer : allCustomers) {
+            if(findCustomer.getEmail().equals(customer.getEmail())) {
+                throw new RuntimeException("Email already exists!!");
+            }
+        }
+        customerRepository.saveAndFlush(customer); 
         //todo: check if fraudster
-        customerRepository.save(customer);
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject("http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId());
+
+        if (fraudCheckResponse.isFraudster()) {
+            throw new RuntimeException("Man's a fraudster!!");
+        }
         //todo:  send notification
     }
 }
